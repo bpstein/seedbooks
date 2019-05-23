ef19f37ed013d73b312860a27a3d0d3620dedecb
Ben Stein <bumstain@gmail.com>
2019-05-23 23:18:49 +0800

    Refactored landing page into painto partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get "/pages/:page" => "pages#show"
+  get '/pages/*page' => 'pages#show'
+  get 'pages/howitworks'
+  get 'pages/pricing'
+  get 'pages/faq'
 end

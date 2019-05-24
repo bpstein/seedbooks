ac988d40d1bb744653ca933380101e9f7c6e559f
Ben Stein <bumstain@gmail.com>
2019-05-24 23:25:33 +0800

    Refactored partials on home page

--- spec/features/pages_feature_spec.rb
+++ spec/features/pages_feature_spec.rb
@@ -11,7 +11,25 @@ feature 'Visitor can view navigation links' do
   end
 
   context 'Visitor can navigate to other pages via links in navbar' do
-    it 'should allow access via publicly accessible links' do
+    it 'should allow user to go to home page by clicking logo' do 
+      visit('/')
+      click_on 'SeedBooks'
+      expect(current_path).to eq('/')      
+    end
+
+    it 'should allow user to access How It Works page' do
+      visit('/')
+      click_on 'How it Works'
+      expect(current_path).to eq('/pages/howitworks')      
+    end
+
+    it 'should allow user to access Pricing page' do
+      visit('/')
+      click_on 'Pricing'
+      expect(current_path).to eq('/pages/pricing')      
+    end
+
+    it 'should allow user to access FAQ page' do
       visit('/')
       click_on 'FAQ'
       expect(current_path).to eq('/pages/faq')      
